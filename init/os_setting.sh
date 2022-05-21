4490789438ecc38960c660ed5214fb6f866008ad
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-05-22 01:22:56 +0900

    add OS setting

--- init/os_setting.sh
+++ init/os_setting.sh
@@ -7,4 +7,17 @@ if [ "$(uname)" != 'Darwin' ]; then
 fi
 
 echo "Setting macOS..."
-#TODO
+
+# ---------- Menu bar ---------- #
+# 日付表示を M月d日(EEE) H:mmに変更
+defaults write com.apple.menuextra.clock "DateFormat" -string "M\u6708d\u65e5(EEE)  H:mm"
+
+# ---------- Dock ---------- #
+# Dockを右に表示
+defaults write com.apple.dock "orientation" -string "right"
+# Dockの「最近使用した項目」を表示しない
+defaults write com.apple.dock "show-recents" -bool "false"
+
+killall Dock
+
+# TODO
