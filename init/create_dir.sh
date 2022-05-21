2f4552fde6a521ecc9049800216ff50485851e98
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-05-22 01:22:41 +0900

    add output log

--- init/create_dir.sh
+++ init/create_dir.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 set -eu
 
-mkdir -vp $HOME/dev
-mkdir -vp $HOME/workspace
+echo "Creating directories..."
+echo "create $HOME/dev/traP"
+mkdir -p $HOME/dev/traP
+
+echo "create $HOME/dev/toshi-pono"
+mkdir -p $HOME/dev/toshi-pono
+
+echo "create $HOME/dev/event"
+mkdir -p $HOME/dev/event
+
+echo "create $HOME/workspace/univ"
+mkdir -p $HOME/workspace/univ
